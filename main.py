0a6ca513b1c5711fc6e81f76e01dab6fcda3eae2
juanchera29 <juanfluorez@gmail.com>
2025-08-12 22:46:41 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from routers.main_router import router as main_router
 
 app = FastAPI()
 
-
+# Ruta raíz para prueba simple
+@app.get("/")
+async def root():
+    return {"message": "Servidor backend de música funcionando"}
 
 # Incluir todas las rutas
 app.include_router(main_router)
-
