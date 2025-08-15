f59fe7a5e94a51c33a5faabeb5556f4f6c2080c1
juanchera29 <juanfluorez@gmail.com>
2025-08-15 18:43:47 -0500

    Delete ver_historial.py

--- ver_historial.py
+++
@@ -1,7 +0,0 @@
-from app.database import SessionLocal, SearchHistory
-
-db = SessionLocal()
-resultados = db.query(SearchHistory).all()
-
-for item in resultados:
-    print(item.query, item.timestamp)
